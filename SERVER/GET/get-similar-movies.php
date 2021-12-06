7c410ee93e3b725f8d88153bf846638fff5f9480
Elaskuss <ivan.k@live.se>
2021-12-06 15:18:16 +0100

    Skiten funkar inte
    Fattar inte varför

--- SERVER/GET/get-similar-movies.php
+++ SERVER/GET/get-similar-movies.php
@@ -1,26 +1,31 @@
-<!-- 
+<?php
+
+/*
     * Vi får MovieID, kalla på TM-database
     * Skicka tillbaka data till klienten
 
-    
- -->
- <?php
+*/
 
-require_once "functions.php";
+require_once "access-control.php";
+require_once "../functions.php";
 
-$method = $_SERVER["REQUEST_METHOD"];
-
-if($method == "GET" && $GET["similar-movies"]){
-    if(!is_numeric($GET["similar-movies"])){
-        send(["Error" => 404]);
+if($method === "GET" && isset($_GET["movieID"])){
+    if(!is_numeric($_GET["movieID"])){
+        send(["message" => "Wrong method"], 400);
+        exit();
+    }
+    else{
+        getSimilarMovies();
     }
 }
 
 
-function getSimilarMovies($movie_id){
-
+function getSimilarMovies(){
+    
+    $movieID = $_GET["movieID"];
+    
     // The URL that we want to GET.
-    $url = "https://api.themoviedb.org/3//movie/{$movie_id}/similar?api_key=f5c0e0db147d0e6434391f3ff153b6a8";
+    $url = "https://api.themoviedb.org/3/movie/$movieID/similar?api_key=f5c0e0db147d0e6434391f3ff153b6a8&language=en-US&page=1";
 
     //Use file_get_contents to GET the URL in question.
     $contents = file_get_contents($url);
@@ -31,8 +36,11 @@ function getSimilarMovies($movie_id){
             ["message" => json_decode($contents, true)]
         );
     }
-    
-
+    else{
+        send(
+            ["message" => "something went wrong"], 404
+        ); 
+    }
 }
 
 
