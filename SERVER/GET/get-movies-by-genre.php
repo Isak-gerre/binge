21e42fdbebb366088adb36ec3c14fc750c245012
linaanderssoon <82369814+linaanderssoon@users.noreply.github.com>
2021-12-06 16:38:14 +0100

    Hämta lista av alla genres, hämta filmer baserat på en genre

--- SERVER/GET/get-movies-by-genre.php
+++ SERVER/GET/get-movies-by-genre.php
@@ -1,3 +1,37 @@
-<!-- 
-    
- -->
+<?php
+require_once "access-control.php";
+require_once "../functions.php";
+
+$method = $_SERVER["REQUEST_METHOD"];
+
+// Gets movies by genre
+function getMoviesByGenres($genre) {
+    $page1 = "https://api.themoviedb.org/3/discover/movie?api_key=f5c0e0db147d0e6434391f3ff153b6a8&language&language=en-US&sort_by=popularity.desc&with_genres=$genre&page=1";
+    $page2 = "https://api.themoviedb.org/3/discover/movie?api_key=f5c0e0db147d0e6434391f3ff153b6a8&language&language=en-US&sort_by=popularity.desc&with_genres=$genre&page=2";
+
+    $dataPage1 = file_get_contents($page1);
+    $dataPage2 = file_get_contents($page2);
+
+    $allPages = [
+        "page1" => json_decode($dataPage1, true),
+        "page2" => json_decode($dataPage2, true)
+    ];
+
+    send($allPages);
+}
+
+if($method == "GET") {
+
+    if(isset($_GET["genre"])) {
+        $genre = $_GET["genre"];
+
+        getMoviesByGenres($genre);
+    }
+
+} else {
+
+    send(
+        ["message" => "Method not allowed"], 400
+    );
+}
+?>
