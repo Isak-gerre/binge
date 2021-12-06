c847986bfa6d07d38b951f45137c0b6ecef827da
Isak <isak.gerre@gmail.com>
2021-12-06 15:04:54 +0100

    Update get-additional-movieInfo.php

--- SERVER/GET/get-additional-movieInfo.php
+++ SERVER/GET/get-additional-movieInfo.php
@@ -1,5 +1,49 @@
-<!-- 
-    * Hämta information om filmens streaming tjänster, 
-    cast plus director
-    * Skicka tillbaka datan 
- -->
+<?php
+
+require_once "access-control.php";
+require_once "../functions.php";
+
+$method = $_SERVER["REQUEST_METHOD"];
+
+
+if ($method === "GET") {
+    if (isset($_GET["movieID"])) {
+        getAdditionalMovieInfo();
+    } else {
+        send(
+            ["message" => "MovieID was not sent"],
+            400
+        );
+    };
+} else {
+    send(
+        ["message" => "Method not allowed"],
+        400
+    );
+};
+
+
+
+function
+getAdditionalMovieInfo()
+{
+    $movieID = $_GET["movieID"];
+    $credits = "https://api.themoviedb.org/3/movie/$movieID/credits?api_key=f5c0e0db147d0e6434391f3ff153b6a8";
+    $providers = "https://api.themoviedb.org/3/movie/$movieID/watch/providers?api_key=f5c0e0db147d0e6434391f3ff153b6a8";
+
+    //Use file_get_contents to GET the URL in question.
+    $contentsCredits = json_decode(file_get_contents($credits), true);
+    $contentsProviders = json_decode(file_get_contents($providers), true);
+
+    $message = [
+        "credits" => $contentsCredits,
+        "providers" => $contentsProviders,
+    ];
+
+    //If $contents is not a boolean FALSE value.
+    if ($message !== false) {        //Print out the contents.
+        send(
+            ["message" => $message]
+        );
+    };
+};
