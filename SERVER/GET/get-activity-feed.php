921804f6059c8eac4e2658b2d03e5fa0f43f95d4
linaanderssoon <82369814+linaanderssoon@users.noreply.github.com>
2021-12-07 09:32:47 +0100

    Update get-activity-feed.php

--- SERVER/GET/get-activity-feed.php
+++ SERVER/GET/get-activity-feed.php
@@ -12,3 +12,20 @@
 
     }
  -->
+
+
+<!-- 
+  1. Ta emot en array av id: [following]
+  2. Gå igenom alla id och hämta id:ts aktiviteter från activitys.json
+  3. Spara i activity ids i array
+ -->
+<?php
+
+function getFriendsActivitieIDs($followingIDs) {
+  $activities = json_decode(file_get_contents("acitivities.json"), true)["activities"];
+  
+
+}
+ 
+ 
+?>
