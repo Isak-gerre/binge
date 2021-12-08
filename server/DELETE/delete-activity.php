d84f169534806273f440b6e7ed0c0a1f61984c80
eelsalindh <elsalindh@gmail.com>
2021-12-08 10:17:17 +0100

    Delete.php
    Nu funkar den!

--- server/DELETE/delete-activity.php
+++ server/DELETE/delete-activity.php
@@ -6,7 +6,7 @@ require_once "../GET/access-control.php";
 require_once "../functions.php";
 
 $method = $_SERVER["REQUEST_METHOD"];
-$activityData = loadJson("../DATABASE/activities.json");
+$activityData = loadJSON("../DATABASE/activities.json");
 
 error_reporting(-1);
 
@@ -18,51 +18,24 @@ if($method === "DELETE"){
     //Kolla så ett id skickats med
     if(isset($requestData["id"])) {
         $activityID = $requestData["id"];
-        $found = false;
 
-        $placement = array_search($activityID, $activityData["activities"]);
 
-        if(in_array($activityID, $activityData["activities"])){
-            $found = true;
-            array_splice($activityData["activities"], $placement, 1);
-            
-        }
-
-        // foreach($activityData["activities"] as $index => $activity){
-        //     //FRÅGA: Såhär har jag ju alltid gjort innan,
-        //     //men då har vi inte haft id't som "namn" på objektet.
-        //     //Ska det se annorlunda ut då? Behöver jag ens göra 
-        //     //foreachen såhär då?
-        //     if($activity == $activityID){
-        //         $found = true;
-        //         array_splice($activityData["activities"], $index, 1);
-        //         break;
-        //     }
-        // }
-
-        // Borde jag göra såhär istället??
-        // foreach($activityData["activities"] as $index) {
-        //     if(in_array($id, $activityData["activities"])){
-        //         $found = true;
-        //         array_splice($activityData["activities"], $index, 1);
-        //         break;
-        //     }
-        // }
-
-        //Om found är false så har inte aktiviteten hittats
-        if($found === false) {
-            sendJson([
+        
+        if(array_key_exists("$activityID", $activityData["activities"])) {
+            unset($activityData["activities"]["$activityID"]);
+        } else {
+            sendJSON([
                 "code" => 2,
                 "message" => "This activity does not exit"
-            ], 404);            
+            ], 404);
         }
 
         // Spara ändringar + felmeddelanden
-        saveJson("../DATABASE/activities.json", $activityData["activities"]);
-        sendJson("Removed activity $activityID.");
+        saveJSON("../DATABASE/activities.json", $activityData);
+        sendJSON("Removed activity $activityID.");
     } else {
         //Om inget id skickats med
-        sendJson([
+        sendJSON([
             "code" => 3,
             "message" => "Id is required if you want to delete activity"
         ], 400);
