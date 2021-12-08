181e65d317a08ca904bbcc2e677091a42457e35d
Elaskuss <ivan.k@live.se>
2021-12-08 14:17:17 +0100

    håller

--- server/POST/create-user.php
+++ server/POST/create-user.php
@@ -17,6 +17,28 @@
     $rawUserData = file_get_contents("php://input");
     $userData = json_decode($rawUserData, true);
 
+
+    if(isset($_FILES["fileToUpload"]["name"])){
+        $target_dir = "uploads/";
+        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+        $target_file = explode(" ", $target_file);
+        var_dump($target_file);
+        var_dump(strtolower(pathinfo($target_file,PATHINFO_EXTENSION)));
+        $uploadOk = 1;
+        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+        // Check if image file is a actual image or fake image
+        if(isset($_POST["submit"])) {
+          $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+          if($check !== false) {
+            echo "File is an image - " . $check["mime"] . ".";
+            $uploadOk = 1;
+          } else {
+            echo "File is not an image.";
+            $uploadOk = 0;
+          }
+        }
+    }
+
     $db = loadJSON("../DATABASE/user.json");
 
         if(alreadyTaken($db["users"], "username", $userData["username"])){
