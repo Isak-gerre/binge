b45214f4a3e350e428558ed1baed47fddd56c618
matildaein <82158772+matildaein@users.noreply.github.com>
2021-12-08 17:01:31 +0100

    delete-user.php
    Tar bort användare och alla aktiviteter gjorda av den användaren.

---
+++ server/DELETE/delete-user.php
@@ -0,0 +1,99 @@
+<?php
+
+/* 
+Tar emot:
+- userID
+
+Returnernar:
+- status: success / error
+- deletedUser / message
+
+
+*/
+
+
+include_once "../access-control.php";
+include_once "../functions.php";
+
+checkMethod("DELETE");
+checkContentType();
+
+$data = file_get_contents("php://input");
+$requestData = json_decode($data, true);
+
+if (!isset($requestData["userID"])) {
+    sendJSON(
+        [
+            "status" => "error",
+            "message" => "Missing userID in body request"
+        ],
+        400
+    );
+}
+
+$userID = $requestData["userID"];
+
+sendJSON(
+    [
+        "status" => "success",
+        "deletedUser" => deleteUser($userID)
+    ]
+);
+
+function deleteUser($userId) {
+    $usersDB = loadJSON("../DATABASE/user.json");
+    $users = $usersDB["users"];
+
+    $found = false;
+
+    foreach ($users as $key => $user) {
+        if ($user["id"] == $userId) {
+            $found = true;
+            $deletedUser = $user;
+            
+            // Kolla om bilden är en avatar eller egen profilbild
+            $profPicFilepath = $user["profile_picture"]["filepath"];
+            // Om det är en egen profilbild, ta bort bilden
+            if (strpos($profPicFilepath, 'PROFILE')) {
+                unlink("../$profPicFilepath");
+            }
+
+            deleteActivities($user["id"]);
+
+            unset($usersDB["users"][$key]);
+        }
+    }
+
+    // Om användaren inte hittas skickas ett felmeddelande
+    if ($found == false) {
+        sendJSON(
+            [
+                "status" => "error",
+                "message" => "User was not found"
+            ],
+            404
+        );
+    }
+
+    saveJSON("../DATABASE/user.json", $usersDB);
+
+    return $deletedUser;
+}
+
+function deleteActivities($userId) {
+
+    $activityData = loadJSON("../DATABASE/activities.json");
+    $activities = $activityData["activities"];
+    
+    // Tar bort de aktiviteter med userID som tillhör den raderade användaren
+    foreach ($activities as $key => $activity) {
+        if ($activity["userID"] == $userId) {
+            unset($activityData["activities"][$key]);
+        }
+    }
+    
+    // Sparar den nya databasen
+    saveJSON("../DATABASE/activities.json",$activityData);
+}
+
+?>
