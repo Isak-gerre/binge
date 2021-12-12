8222af41c561b8e3e3cdede5a5842f01ef99bda6
linaanderssoon <82369814+linaanderssoon@users.noreply.github.com>
2021-12-12 21:47:36 +0100

    början på makefeed

--- KLIENT/scripts/makeFeed.js
+++ KLIENT/scripts/makeFeed.js
@@ -2,3 +2,33 @@
 // Sortera aktiviterna enligt datum
 // Skapa elementen, appenda dem 
 // Kallar på makeMovie profile vid klick. Movie Profile kommer upp.
+
+"use strict";
+
+// Hämta den inloggade användares id
+let loggedInUserID = sessionStorage.getItem("loggedInUserID");
+
+// Hämta den inloggade användares "following" (functions.php)
+async function getFollowing(loggedInUserID) {
+    let loggedInUser = await getUsersById(IDs); // returnerar en användaare
+
+    // Hämta användarens following
+    let following = loggedInUser.following;
+
+    // Kalla på funktion som skapar aktiteterna med id arr
+    makeFriendsActivities(following);
+}
+
+async function makeFriendsActivities(loggedInUserID){
+    // Hämta aktiviteterna från db  (get-activitys.php)
+    let activities = await getFriendsActivities(IDs);
+
+    //stortera aktiviteterna efter datum
+    activities.sort()
+
+    activities.forEach(a => {
+        // skapa divs
+        
+    });
+    
+}
