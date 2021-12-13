830f7e269d1f96db94523ebfc415fbcfc078a2d9
matildaein <82158772+matildaein@users.noreply.github.com>
2021-12-13 10:10:06 +0100

    Uppdaterade till styles.css istället för tempstyle

--- KLIENT/head.php
+++ KLIENT/head.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- LÄNKA TILL CSS, FONTS -->
-    <link rel="stylesheet" href="styles/tempstyles.css">
+    <link rel="stylesheet" href="styles/styles.css">
     <title>Document</title>
 </head>
 <body>
