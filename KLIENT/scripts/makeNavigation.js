48f2c68b3ddda3ac3b18a4afe5135e5bbadc5454
eelsalindh <elsalindh@gmail.com>
2021-12-13 18:15:31 +0100

    Navigation
    Inte helt klar men:
    - ikoner inlagda
    - länkar
    - lite css i styles scss

--- KLIENT/scripts/makeNavigation.js
+++ KLIENT/scripts/makeNavigation.js
@@ -18,17 +18,42 @@ function makeUpperNav(page) {
 
  
     //content
-    navLeft.innerHTML = "<a href='index.php'>Back</a>";
-    navMiddle.innerHTML =  "<a href='index.php'>Home/logo</a>";
-    navRight.innerHTML =  "<a href='index.php'>Search</a>";
+    navLeft.innerHTML = `<a href='index.php'><img src='../styles/images/iconBack.png' class ='navImg' alt='Back'></a>`;
+    navMiddle.innerHTML =  `<a href='index.php'><img src='../styles/images/bForBingy.png' class ='navImg' alt='Back'></a>`;
+    navRight.innerHTML =  `<a href='index.php'><img src='../styles/images/iconSearch.png' class ='navImg' alt='Search'></a>`;
  
     //append
     upperNav.append(navLeft, navMiddle, navRight);
     document.getElementById("wrapper").append(upperNav);
 }
-
 makeUpperNav();
 
 function makeLowerNav(page) {
+
+    //create elements
+    let lowerNav = document.createElement('nav');
+    let lowerNavLeft = document.createElement('div');
+    let lowerNavMiddle = document.createElement('div');
+    let lowerNavRight = document.createElement('div');
     
+    //classes
+    lowerNav.className = "lowerNav";
+    lowerNavLeft.className = "lowerNavLeft";
+    lowerNavMiddle.className = "lowerNavMiddle";
+    lowerNavRight.className = "lowerNavRight";
+
+    //content
+    lowerNavLeft.innerHTML = `<a href='feed.php'><img src='../styles/images/iconHome.svg' class ='navImg' alt='Home'></a>`;
+    lowerNavMiddle.innerHTML =  `<a href='explore.php'><img src='../styles/images/iconExplore.png' class ='navImg' alt='Explore'></a>`;
+    lowerNavRight.innerHTML =  `<a href='profile.php'><img src='../styles/images/iconProfile1.png' class ='navImg' alt='Profile'></a>`;
+    
+    if(page == 'home'){
+        lowerNavLeft.classList.add('current');
+    }
+
+    //append
+    lowerNav.append(lowerNavLeft, lowerNavMiddle, lowerNavRight);
+    document.getElementById("wrapper").append(lowerNav);
 }
+
+makeLowerNav('home');
