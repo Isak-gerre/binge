2dcad6cc7e68d56b306ca724f057d26b7405487c
matildaein <82158772+matildaein@users.noreply.github.com>
2021-12-13 13:10:35 +0100

    Uppdaterad makeUserProfile

--- KLIENT/scripts/makeUserProfile.js
+++ KLIENT/scripts/makeUserProfile.js
@@ -36,23 +36,40 @@ Om det är någon annans profil:
 // Stats:
 //
 
-const loggedInUser = sessionStorage.userId;
+// Inloggad användare
+const loggedInUserId = Number(sessionStorage.getItem('userID'));
+
+// Fasta html-variabler
+const proPicCont = document.getElementById('profilePic');
+const uNameCont = document.getElementById('username');
+const settingOrPlus = document.getElementById('settingOrPlus');
+const followersCont = document.getElementById('followers');
+const followingCont = document.getElementById('following');
+
 
 createProfilePage();
 
 async function createProfilePage() {
-    let userId = checkUser();
-    // const userId = checkUser();
+    const loggedInUserInfo = await getUserInfo(loggedInUserId);
+    
+    let urlUserId = getUserFromUrl();
 
-    let userInfo = await getUserInfo(userId); 
-    createProfileHeader(userInfo);
+    if (urlUserId !== null) {
+        let userInfo = await getUserInfo(urlUserId);
+
+        let loggedInUserFollow = loggedInUserInfo.following;
+        let following = loggedInUserFollow.some(e => e == urlUserId);
+
+        createProfileHeader(userInfo, following);
+    } else {
+        createProfileHeader(loggedInUserInfo, null, true);
+    }
+
 
     // createProfileFeed()?
 
 
 }
-
-
 
 function getUserFromUrl() {
     let getParams = window.location.search;
@@ -72,34 +89,93 @@ async function getUserInfo(userId) {
     return userInfo[0];
 }
 
-async function createProfileHeader(userInfo) {
-    
-    const proPicCont = document.getElementById('profilePic');
-    const uNameCont = document.getElementById('usernameDiv');
-    const followersCont = document.getElementById('followers');
-    const followingCont = document.getElementById('following');
+async function createProfileHeader(user, isFollowing, settings = null) {
 
-    let username = userInfo.username.toLowerCase();
-
+    let username = user.username.toLowerCase();
+    uNameCont.textContent = "@" + username;
     let profilePic = document.createElement('img');
 
     // vi behöver ett url här va
-    profilePic.src = `http://localhost:8001${userInfo.profile_picture.filepath}`;
+    profilePic.src = `http://localhost:8001/${user.profile_picture.filepath}`;
     
-    let followers = userInfo.followers;
-    let following = userInfo.following;
-    let nrOffFollowers = followers.length;
-    let nrOffFollowing = following.length;
+    let followers = user.followers;
+    let following = user.following;
+    let nrOfFollowers = followers.length;
+    let nrOfFollowing = following.length;
+    let profileButton = document.createElement('button');
 
+    if (isFollowing !== null) {
+        if (isFollowing) {
+            profileButton.textContent = "unfollow";
+        } else if (!isFollowing) {
+            profileButton.textContent = "follow";
+        }   
+    }
+    
+    if (settings == true) {
+        profileButton.textContent = "settings";
+    }
+
+    profileButton.addEventListener('click', async function () {
+
+        if (profileButton.textContent == "unfollow") {
+            isFollowing = false;
+
+            profileButton.textContent = "follow";
+            let response = await followPatch(loggedInUserId, user.id);
+            console.log(response);
+
+            nrOfFollowers -= 1;
+            followersCont.textContent = nrOfFollowers;
+            
+
+        } else if (profileButton.textContent == "follow") {
+            isFollowing = true;
+
+            profileButton.textContent = "unfollow";
+            let response = await followPatch(loggedInUserId, user.id);
+            console.log(response);
+
+            nrOfFollowers += 1;
+            followersCont.textContent = nrOfFollowers;
+            
+
+        } else if (profileButton.textContent == "settings") {
+            openSettings(user.id);
+        }
+    })
+    
     proPicCont.append(profilePic);
-    uNameCont.append('@' + username);
-    followersCont.append(nrOffFollowers);
-    followingCont.append(nrOffFollowing);
+    settingOrPlus.append(profileButton);
+    followersCont.append(nrOfFollowers);
+    followingCont.append(nrOfFollowing);
 
 }
 
-async function getActivityInfo(userId) {
+async function followPatch(mainUserID, friendsUserID) {
+    let infoToSend = {
+        userID: mainUserID, 
+        friendsUserID: friendsUserID
+    }
+
+    const response = await fetch(new Request("http://http://localhost:8001/PATCH/update-user.php", {
+        method: "PATCH",
+        body: JSON.stringify(infoToSend),
+        headers: {
+            "Content-type": "application/json"
+        }
+    }));
+
+    let data = await response.json();
+    console.log(data);
+}
+
+// async function getActivityInfo(userId) {
 
 
+// }
+
+function openSettings(userId) {
+    console.log(userId);
 }
 
