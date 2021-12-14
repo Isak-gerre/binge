2b855f27a6930c90bde3a5f26046310e839e509d
Isak <isak.gerre@gmail.com>
2021-12-14 08:55:45 +0100

    moved files

--- KLIENT/scripts/stats.js
+++ KLIENT/scripts/stats.js
@@ -4,4 +4,16 @@ Create stats
 
 */
 
-const myChart = new Chart(ctx);
+const data = {
+  labels: ["Red", "Blue", "Yellow"],
+  datasets: [
+    {
+      label: "My First Dataset",
+      data: [300, 50, 100],
+      backgroundColor: ["rgb(255, 99, 132)", "rgb(54, 162, 235)", "rgb(255, 205, 86)"],
+      hoverOffset: 4,
+    },
+  ],
+};
+
+const myChart = new Chart(data);
