bd63d543be9c3e009ef5fbc9aedbdf141c0d9647
Isak <isak.gerre@gmail.com>
2021-12-14 10:43:41 +0100

    working

--- KLIENT/scripts/stats.js
+++ KLIENT/scripts/stats.js
@@ -5,15 +5,24 @@ Create stats
 */
 
 const data = {
-  labels: ["Red", "Blue", "Yellow"],
-  datasets: [
-    {
-      label: "My First Dataset",
-      data: [300, 50, 100],
-      backgroundColor: ["rgb(255, 99, 132)", "rgb(54, 162, 235)", "rgb(255, 205, 86)"],
-      hoverOffset: 4,
+  type: "doughnut",
+  options: {
+    layout: {
+      padding: 30,
     },
-  ],
+  },
+  data: {
+    labels: ["Action", "Romance", "Horror", "Comedy"],
+    datasets: [
+      {
+        label: "My First Dataset",
+        data: [300, 50, 100, 400],
+        backgroundColor: ["rgb(255, 99, 132)", "rgb(54, 162, 235)", "rgb(255, 205, 86)", "rgb(123, 22, 143)"],
+        hoverOffset: 4,
+      },
+    ],
+  },
 };
 
-const myChart = new Chart(data);
+let ctx = document.getElementById("ctx").getContext("2d");
+const myChart = new Chart(ctx, data);
