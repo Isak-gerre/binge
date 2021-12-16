534c9951128f90567eddd0b64dbe0e94de9d6dd2
eelsalindh <elsalindh@gmail.com>
2021-12-16 14:17:09 +0100

    nav i body ist

--- KLIENT/scripts/makeNavigation.js
+++ KLIENT/scripts/makeNavigation.js
@@ -58,7 +58,7 @@ function makeUpperNav() {
     //append
     upperNav.append(navLeft, navMiddle, navRight);
     navRight.append(searchImg);
-    document.getElementById("wrapper").append(upperNav);
+    document.body.append(upperNav);
 }
 makeUpperNav();
 
@@ -98,7 +98,7 @@ function makeLowerNav(page) {
 
     //append
     lowerNav.append(backLowerNav, lowerNavLeft, lowerNavMiddle, lowerNavRight);
-    document.getElementById("wrapper").append(lowerNav);
+    document.body.append(lowerNav);
 }
 
 makeLowerNav('home');
