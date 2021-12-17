3d2d705e06a207e901f446ae4f9ea67a7d57631a
matildaein <82158772+matildaein@users.noreply.github.com>
2021-12-17 11:41:53 +0100

    Tog bort style-länk

--- KLIENT/head.php
+++ KLIENT/head.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- LÄNKA TILL CSS, FONTS -->
     <link rel="stylesheet" href="styles/styles.css">
-    <link rel="stylesheet" href="styles/styles.scss">
     <title>Document</title>
 </head>
 <body>
