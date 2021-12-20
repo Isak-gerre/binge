3ae98f2f0b898813e69148480f6b32bc4360cffb
Isak <isak.gerre@gmail.com>
2021-12-20 08:04:58 +0100

    uppdate

--- KLIENT/scripts/search.js
+++ KLIENT/scripts/search.js
@@ -5,61 +5,57 @@ Add filter that actiate on button press
 
 */
 
-let searchType = 'all';
+let searchType = "all";
 
+async function searchFunction() {
+  let input = document.getElementById("searchField");
 
-async function searchFunction(){
-    let input = document.getElementById('searchField');
+  if (input.value !== "") {
+    let searchResults = await getSearchResults(searchType, input.value);
+    let movieList = document.querySelector(".movieList");
+    movieList.innerHTML = "";
+    console.log(searchResults);
 
-    if (input.value !== '') {
-        let searchResults = await getSearchResults(searchType, input.value);
-        let movieList = document.querySelector('.movieList');
-        movieList.innerHTML = '';
-        // console.log(searchResults);
+    // searchResults.results.forEach(async function(result){
+    //     // getMovieDOMElement(result);
 
-        searchResults.results.forEach(async function(result){
-            // getMovieDOMElement(result);
-            
-            let movieBanner = await makeMovieBanner(result.id);
-            movieBanner.classList.add(input.value);
-            movieList.append(movieBanner);
-        });
-    }
+    //     let movieBanner = await makeMovieBanner(result.id);
+    //     movieBanner.classList.add(input.value);
+    //     movieList.append(movieBanner);
+    // });
+  }
 }
 
-function makeSearchOverlay(){
-    let searchOverlay = document.createElement('div');
-    let movieList = document.createElement('div');
-    let pillsDiv = document.createElement('div');
-    let pillMovie = document.createElement('div');
-    let pillCast = document.createElement('div');
+function makeSearchOverlay() {
+  let searchOverlay = document.createElement("div");
+  let movieList = document.createElement("div");
+  let pillsDiv = document.createElement("div");
+  let pillMovie = document.createElement("div");
+  let pillCast = document.createElement("div");
 
+  searchOverlay.className = "searchOverlay";
+  movieList.className = "movieList";
+  pillsDiv.className = "pillsDiv";
+  pillMovie.className = "pill pillMovie";
+  pillCast.className = "pill pillCast";
 
-    searchOverlay.className = "searchOverlay";
-    movieList.className = "movieList";
-    pillsDiv.className = "pillsDiv";
-    pillMovie.className = "pill pillMovie";
-    pillCast.className = "pill pillCast";
+  // searchOverlay.innerHTML = 'search by:'
+  pillsDiv.innerHTML = "search by:";
+  pillMovie.innerHTML = "Movies";
+  pillCast.innerHTML = "Cast";
 
-    // searchOverlay.innerHTML = 'search by:'
-    pillsDiv.innerHTML = 'search by:'
-    pillMovie.innerHTML = 'Movies'
-    pillCast.innerHTML = 'Cast'
+  searchOverlay.prepend(pillsDiv);
+  pillsDiv.append(pillMovie, pillCast);
+  searchOverlay.append(movieList);
+  document.getElementById("wrapper").append(searchOverlay);
 
-    searchOverlay.prepend(pillsDiv);
-    pillsDiv.append(pillMovie, pillCast);
-    searchOverlay.append(movieList);
-    document.getElementById('wrapper').append(searchOverlay);
+  pillMovie.addEventListener("click", () => {
+    searchType = "movie";
+    pillMovie.classList.toggle("filtered");
+  });
 
-
-    pillMovie.addEventListener('click', () => {
-        searchType = 'movie';
-        pillMovie.classList.toggle('filtered');
-    })
-
-    pillCast.addEventListener('click', () => {
-        searchType = 'cast';
-        pillCast.classList.toggle('filtered');
-    })
-
+  pillCast.addEventListener("click", () => {
+    searchType = "cast";
+    pillCast.classList.toggle("filtered");
+  });
 }
