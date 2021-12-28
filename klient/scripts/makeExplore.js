1ae931508d57efa00d120fefb4f0899304c6ba6d
Isak <isak.gerre@gmail.com>
2021-12-28 09:25:15 +0100

    revert

--- klient/scripts/makeExplore.js
+++ klient/scripts/makeExplore.js
@@ -15,8 +15,9 @@ if(getParamFromUrl("movieID")){
     }, 1000);
 
     //FRIENDS RECENTLY WATCHED
-    executeFriendsActivities();
+
     //GENRED
+    executeFriendsActivities();
     makeGenreBanner();
 
 
