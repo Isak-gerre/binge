5ad42152b4e27294282efaec8302eeddc890b4fa
Elaskuss <ivan.k@live.se>
2022-01-06 23:14:50 -0800

    api remove

---
+++ SERVER/GET/get-regions.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once "../access-control.php";
+require_once "../functions.php";
+
+$method = $_SERVER["REQUEST_METHOD"];
+
+if($method != "GET"){
+    sendJSON(["message" => "Method not allowed"]);
+}
+
+$url = "http://api.themoviedb.org/3/genre/movie/list?api_key=f5c0e0db147d0e6434391f3ff153b6a8&language=en-US";
+
+https://api.themoviedb.org/3/watch/providers/regions?api_key=f5c0e0db147d0e6434391f3ff153b6a8
+$fetch = file_get_contents($url);
+
+$data = json_decode($fetch);
+
+inspect($data);
+
+// sendJSON($data);
+
+?>
