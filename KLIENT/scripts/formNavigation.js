5010a3ff3d3a123a9d42a12ed594e42ac5d53bbc
Elaskuss <ivan.k@live.se>
2022-01-12 00:19:40 -0800

    upt2

--- KLIENT/scripts/formNavigation.js
+++ KLIENT/scripts/formNavigation.js
@@ -22,7 +22,9 @@ function checkStuff() {
                 }, 1000)
             }
         });
-        next = samePass;
+        if(!check1 || !check2){
+            next = false;
+        }
 
         if (next) {
             document.getElementById("createUserP1").style.display = "none";
@@ -59,24 +61,57 @@ document.getElementById("registerButton").addEventListener("click", () => {
 });
 
 
-let samePass = false;
+let check1 = false;
+let check2 = false;
+
+
+document.querySelector("#password2").addEventListener("keyup", () => {
+    let password = document.querySelector("#password2");
+    if(password.value.length == 0){
+        document.querySelector("#createUserP1 div:nth-child(4) > label").textContent = "Password *";
+        check1 = false;
+    }
+    else if(password.value.length < 8){
+        document.querySelector("#createUserP1 div:nth-child(4) > label").textContent = "Password has to be atleast eight characters";
+        document.querySelector("#password2").parentElement.style.border = "2px solid red";
+        document.querySelector("#password2").style.color = 'Red';
+        check1 = false;
+    }
+    else{
+        document.querySelector("#password2").parentElement.style.border = "2px solid green";
+        document.querySelector("#password2").style.color = 'Green';
+        setTimeout(() => {
+            document.querySelector("#password2").parentElement.removeAttribute("style");
+            document.querySelector("#password2").removeAttribute("style");
+        }, 1000)
+        document.querySelector("#createUserP1 div:nth-child(4) > label").textContent = "Password *";
+        check1 = true;
+    }
+});
+
 
 document.querySelector("input[name=confirm_password]").addEventListener("keyup", () => {
-
     let check = document.querySelector("#password2").value;
     let value = document.querySelector("input[name=confirm_password]").value;
 
     if (value === check) {
-        samePass = true;
+        check2 = true;
         document.querySelector("#createUserP1 div:nth-child(5) > label").textContent = "Confirm Password *";
-        document.querySelector("input[name=confirm_password]").parentElement.removeAttribute("style");
-        document.querySelector("input[name=confirm_password]").removeAttribute("style");
+
+        document.querySelector("input[name=confirm_password]").parentElement.style.border = "2px solid green";
+        document.querySelector("input[name=confirm_password]").style.color = 'Green';
+        setTimeout(() => {
+            document.querySelector("input[name=confirm_password]").parentElement.removeAttribute("style");
+            document.querySelector("input[name=confirm_password]").removeAttribute("style");
+        }, 1000)
+
+        
     }
     else {
         document.querySelector("input[name=confirm_password]").parentElement.style.border = "2px solid red";
         document.querySelector("#createUserP1 div:nth-child(5) > label").textContent = "Passwords do not match*";
         document.querySelector("input[name=confirm_password]").style.color = 'Red';
-        samePass = false;
+        check2 = false;
     }
 });
 
